9fa55fcb87f60991bc7b2117ac3075e170a9bd0b
Hithesh <hithesh@users.noreply.github.com>
2016-07-14 14:55:41 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Core;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        //private static string connectionstring = "127.0.0.1:27017";
+        //private static string connectionstring = "mongodb://oodlyadmin:ZHS4CHOE2JPUK9EEOJSKI6Q8UUYTG1G38URQ4XUH@candidate.5.mongolayer.com:10613,candidate.6.mongolayer.com:10495/Oodly-classic-new";
+        private static MongoClient client = new MongoClient();
+        private static IMongoDatabase database = client.GetDatabase("test");
+        public static IMongoCollection<BsonDocument> Users = database.GetCollection<BsonDocument>("users");
+        public static void Main(string[] args)
+        {
+            testing().Wait();
+            Console.WriteLine("Query results end here!!!");
+        }
+        public static async Task testing()
+        {
+            var collection = database.GetCollection<BsonDocument>("restaurants");
+            //var filter = Builders<BsonDocument>.Filter.Eq("grades.grade", "A");
+            //var result =  await collection.Find(filter).ToListAsync();
+            //Console.WriteLine(result.Count);
+            var filter = new BsonDocument();
+            var count = 0;
+            using (var cursor = await collection.FindAsync(filter))
+            {
+                while (await cursor.MoveNextAsync())
+                {
+                    var batch = cursor.Current;
+                    foreach (var document in batch)
+                    {
+                        Console.WriteLine(document.Names);
+                        count++;
+                    }
+                }
+            }*/
+        }
+    }
+}
